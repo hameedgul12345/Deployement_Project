ad83c9ce4510defc78e216a5fbde23accd96943f
Hameed Gul <hamidkhan31590@gmail.com>
2025-01-25 15:17:29 +0500

    Adding Links Components

--- src/assets/components/Links.jsx
+++ src/assets/components/Links.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Links from './Links'
-function Home() {
+
+function Links() {
   return (
    <>
-   <h1>Home</h1>
-  <Links/>
-   
+   <Link to={'/'}>Home</Link>
    
    </>
   )
 }
 
-export default Home
+export default Links
